233b8bdc919924bc7829e4978cb4950b549c8bdb
Adam <adamlenter1@gmail.com>
2021-10-15 12:33:08 -0400

    commit tree 9618775a46e0c2ea1a615021959fb19e23314325

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(firstName, email) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": firstName, 
+            "email": email
+        })
+      };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(response => response.json())
+    .then(body => addNewContact(body))
+    .catch(function (error){
+        console.log(error)
+        alert("Unauthorized Access")
+        document.body.append(error.message)
+    })
+}
+
+
+function addNewContact(newUser) {
+    document.body.append(newUser.id)
+}
+
+submitData("Name", "name@name.com");
